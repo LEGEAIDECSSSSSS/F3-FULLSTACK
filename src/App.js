fd43f38a06da7c40243896fc95acb5ead23e72be
Crosdel Emuedo <your-email@example.com>
2025-10-16 11:02:54 +0100

    picture

--- src/App.js
+++ src/App.js
@@ -12,36 +12,36 @@ import About from "./components/About";
 import Footer from "./components/Footer";
 import LibraryPage from "./pages/LibraryPage";
 import { LibraryProvider, useLibrary } from "./context/LibraryContext";
-
+import bg_dark from "./Images/bg_dark.jpg";
 const categories = [
   {
     title: "New Releases",
     books: [
-      { id: 1, title: "The Silent Heir", img: "https://picsum.photos/300/450?random=1", price: 10 },
-      { id: 2, title: "Whispers of Dawn", img: "https://picsum.photos/300/450?random=2", price: 12 },
-      { id: 3, title: "Echoes of Time", img: "https://picsum.photos/300/450?random=3", price: 14 },
-      { id: 4, title: "Shadowborn", img: "https://picsum.photos/300/450?random=4", price: 11 },
-      { id: 5, title: "Moonlight Prophecy", img: "https://picsum.photos/300/450?random=5", price: 13 },
+      { id: 1, title: "The Silent Heir", img: bg_dark, price: 10 },
+      { id: 2, title: "Whispers of Dawn", img: bg_dark, price: 12 },
+      { id: 3, title: "Echoes of Time", img: bg_dark, price: 14 },
+      { id: 4, title: "Shadowborn", img: bg_dark, price: 11 },
+      { id: 5, title: "Moonlight Prophecy", img: bg_dark, price: 13 },
     ],
   },
   {
     title: "Best Sellers",
     books: [
-      { id: 6, title: "Crimson Skies", img: "https://picsum.photos/300/450?random=6", price: 15 },
-      { id: 7, title: "The Lost Path", img: "https://picsum.photos/300/450?random=7", price: 17 },
-      { id: 8, title: "Rise of the Ember", img: "https://picsum.photos/300/450?random=8", price: 20 },
-      { id: 9, title: "Last Voyage", img: "https://picsum.photos/300/450?random=9", price: 13 },
-      { id: 10, title: "Frozen Legacy", img: "https://picsum.photos/300/450?random=10", price: 18 },
+      { id: 6, title: "Crimson Skies", img: bg_dark, price: 15 },
+      { id: 7, title: "The Lost Path", img: bg_dark, price: 17 },
+      { id: 8, title: "Rise of the Ember", img: bg_dark, price: 20 },
+      { id: 9, title: "Last Voyage", img: bg_dark, price: 13 },
+      { id: 10, title: "Frozen Legacy", img: bg_dark, price: 18 },
     ],
   },
   {
     title: "Fantasy & Adventure",
     books: [
-      { id: 11, title: "Crown of Mist", img: "https://picsum.photos/300/450?random=11", price: 14 },
-      { id: 12, title: "Warden of Fire", img: "https://picsum.photos/300/450?random=12", price: 16 },
-      { id: 13, title: "The Hidden Realm", img: "https://picsum.photos/300/450?random=13", price: 19 },
-      { id: 14, title: "Oathbreaker", img: "https://picsum.photos/300/450?random=14", price: 15 },
-      { id: 15, title: "The Last Mage", img: "https://picsum.photos/300/450?random=15", price: 18 },
+      { id: 11, title: "Crown of Mist", img: bg_dark, price: 14 },
+      { id: 12, title: "Warden of Fire", img: bg_dark, price: 16 },
+      { id: 13, title: "The Hidden Realm", img: bg_dark, price: 19 },
+      { id: 14, title: "Oathbreaker", img: bg_dark, price: 15 },
+      { id: 15, title: "The Last Mage", img: bg_dark, price: 18 },
     ],
   },
 ];
